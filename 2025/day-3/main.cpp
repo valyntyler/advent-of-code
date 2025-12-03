dc2deac6fc02a1016635e86e2d762400185a70e1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-03 20:21:25 +0100

    feat(day-3): part 2 solved !!

--- 2025/day-3/main.cpp
+++ 2025/day-3/main.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 using ll = long long;
 
+void print(vector<int> v) {
+  if (v.size() == 1) {
+    cout << v[0] << endl;
+    return;
+  }
+  cout << v[0] << " ";
+  vector<int> w(v.begin() + 1, v.end());
+  print(w);
+}
+
+ll concat(vector<int> v) {
+  if (v.size() == 1) {
+    return v[0];
+  }
+  vector<int> w(v.begin() + 1, v.end());
+  ll p = pow(10, w.size());
+  ll head = v[0];
+  ll tail = concat(w);
+  return head * p + tail;
+}
+
 vector<int> parse_bank(string s) {
   vector<int> v;
   for (auto c : s) {
@@ -11,28 +32,28 @@ vector<int> parse_bank(string s) {
   return v;
 }
 
-int max_joltage(vector<int> bank) {
-  int bba = 0;
-  int bbb = 0;
-  int bbai;
-
-  for (int i = 0; i < bank.size() - 1; ++i) {
-    int batt = bank[i];
-    if (batt > bba) {
-      bba = batt;
-      bbai = i;
+pair<int, size_t> max_in_range(vector<int> v, size_t left, size_t right) {
+  int best = 0;
+  size_t best_index;
+  for (int i = left; i < right; ++i) {
+    if (v[i] > best) {
+      best = v[i];
+      best_index = i;
     }
   }
+  return {best, best_index};
+}
 
-  for (int i = bbai + 1; i < bank.size(); ++i) {
-    int batt = bank[i];
-    if (batt > bbb) {
-      bbb = batt;
-    }
+ll max_joltage(vector<int> bank, int k) {
+  vector<int> v;
+  size_t left = 0;
+  for (int i = k - 1; i >= 0; --i) {
+    auto [be, bi] = max_in_range(bank, left, bank.size() - i);
+    v.push_back(be);
+    left = bi + 1;
   }
 
-  // cout << bba << " " << bbb << endl;
-  return bba * 10 + bbb;
+  return concat(v);
 }
 
 int main() {
@@ -44,7 +65,7 @@ int main() {
       break;
     }
     vector<int> v = parse_bank(s);
-    ll maxv = max_joltage(v);
+    ll maxv = max_joltage(v, 12);
     cout << s << ": " << maxv << endl;
     sum += maxv;
   }
