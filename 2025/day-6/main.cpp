704517283891070e8c3167e76dae2786f066936f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-09 15:17:11 +0100

    feat(day-06): runs on example

---
+++ 2025/day-6/main.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <sstream>
+
+using namespace std;
+using ll = long long;
+
+enum operation { Add, Mul };
+
+vector<vector<ll>> problems;
+
+ll solve_problem(int j, operation op) {
+  ll total = 0;
+  switch (op) {
+  case Add:
+    total = 0;
+    break;
+  case Mul:
+    total = 1;
+    break;
+  }
+  for (int i = 0; i < problems[0].size() - 1; ++i) {
+    ll n = problems[i][j];
+    switch (op) {
+    case Add:
+      total += n;
+      break;
+    case Mul:
+      total *= n;
+      break;
+    }
+  }
+  return total;
+}
+
+int main() {
+  ll total;
+  ll grand;
+
+  int i = 0;
+  while (!cin.eof()) {
+    string line;
+    getline(cin, line);
+
+    stringstream ss(line);
+    int j = 0;
+
+    if (line == "") {
+      continue;
+    }
+
+    problems.push_back({});
+    while (!ss.eof()) {
+      string token;
+      ss >> token;
+      switch (token[0]) {
+      case '*':
+        problems[i].push_back(solve_problem(j, Mul));
+        break;
+      case '+':
+        problems[i].push_back(solve_problem(j, Add));
+        break;
+      default:
+        problems[i].push_back(stoi(token));
+        break;
+      }
+      ++j;
+    }
+    ++i;
+  }
+
+  for (auto row : problems) {
+    for (auto token : row) {
+      cout << token << " ";
+    }
+    cout << endl;
+  }
+
+  for (int k = 0; k < problems[0].size(); ++k) {
+    grand += problems[problems.size() - 1][k];
+  }
+
+  cout << grand << endl;
+
+  return 0;
+}
