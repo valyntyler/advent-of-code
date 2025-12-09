f5475bfaaf02ce3b2099cbd81862a7d7a042f083
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-12-09 20:51:07 +0100

    wip: day-08

---
+++ 2025/day-8/main.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+struct vector3 {
+  ll x;
+  ll y;
+  ll z;
+
+  ll dist(vector3 other) {
+    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2) +
+                pow(z - other.z, 2));
+  }
+
+  void print() {
+    cout << x << " ";
+    cout << y << " ";
+    cout << z << " ";
+    cout << endl;
+  }
+};
+
+vector<vector3> read_junctions() {
+  vector<vector3> junctions;
+  while (!cin.eof()) {
+    string s;
+    getline(cin, s);
+    if (s == "") {
+      break;
+    }
+    replace(s.begin(), s.end(), ',', ' ');
+    stringstream ss(s);
+    vector3 junc;
+    ss >> junc.x;
+    ss >> junc.y;
+    ss >> junc.z;
+    junctions.push_back(junc);
+  }
+  return junctions;
+}
+
+map<pair<size_t, size_t>, ll> compute_distances(vector<vector3> junctions) {
+  map<pair<size_t, size_t>, ll> distances;
+  for (int i = 0; i < junctions.size(); ++i) {
+    for (int j = i + 1; j < junctions.size(); ++j) {
+      distances[{i, j}] = junctions[i].dist(junctions[j]);
+    }
+  }
+  return distances;
+}
+
+int main() {
+  vector<vector3> junctions = read_junctions();
+
+  // for (auto elem : junctions) {
+  //   elem.print();
+  // }
+
+  map<pair<size_t, size_t>, ll> distances = compute_distances(junctions);
+
+  vector<pair<ll, pair<size_t, size_t>>> distances_swapped;
+  for (auto d : distances) {
+    distances_swapped.push_back({d.second, d.first});
+  }
+
+  sort(distances_swapped.begin(), distances_swapped.end());
+
+  for (auto elem : distances_swapped) {
+    cout << elem.first << endl;
+  }
+
+  size_t size = junctions.size();
+  vector<vector<bool>> connections(size, vector<bool>(size));
+  int k = 10;
+  for (auto d : distances_swapped) {
+    if (k == 0) {
+      break;
+    }
+    connections[d.second.first][d.second.second] = true;
+    connections[d.second.second][d.second.first] = true;
+    --k;
+  }
+
+  for (auto row : connections) {
+    for (auto val : row) {
+      cout << (val ? 'x' : '.') << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
